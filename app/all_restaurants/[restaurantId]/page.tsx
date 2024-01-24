3cc86e49883f97e4c96397993f1494e4090757ac
Abel Mesele <se.abel.mesele@gmail.com>
2024-01-24 17:02:24 +0300

    feat: Add Image upload for restaurant

--- app/all_restaurants/[restaurantId]/page.tsx
+++ app/all_restaurants/[restaurantId]/page.tsx
@@ -2,6 +2,26 @@ import ImageUpload from "@/components/ImageUpload";
 import { itemColumns as columns } from "./columns";
 import { ItemsTable } from "./data-table-items";
 
+async function getRestaurantDetail(restaurantId: string){
+    const base_url = process.env.NEXT_PUBLIC_BASE_URL;
+    if (!base_url) throw new Error('BASE_URL is not defined')
+    const res = await fetch(`${base_url}/restaurants/${restaurantId}`, {
+        method: 'GET',
+    })
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    const responseData = await res.json();
+    console.log('Response Data:', responseData)
+    
+    const restaurantImages = responseData.data.restaurant_images;
+    console.log('These are the item images; ', restaurantImages)
+
+    return restaurantImages;
+}
 
 async function getData(restaurantId: string) {
     const base_url = process.env.NEXT_PUBLIC_BASE_URL;
@@ -14,7 +34,9 @@ async function getData(restaurantId: string) {
         // This will activate the closest `error.js` Error Boundary
         throw new Error('Failed to fetch data')
     }
+
     const responseData = await res.json();
+    console.log('Response Data:', responseData)
 
     // Extract only the id and name from each item
     const extractedData = responseData.data.map(({ id, name }: { id: string, name: string }) => ({ id, name }));
@@ -26,10 +48,17 @@ async function getData(restaurantId: string) {
 
 export default async function Restaurant({ params }: any) {
     const data = await getData(params.restaurantId)
+    const restaurantImages = await getRestaurantDetail(params.restaurantId)
+
     return (
         <main className="bg-white flex flex-col items-center gap-2">
             <div className="text-xl">
-                Restaurant Detail
+                Restaurant Images
+            </div>
+            <div className="flex gap-2">
+                {restaurantImages.map((image: any) => (
+                    <img key={image.id} src={image.url} alt={image.id} width={80} height={'auto'} />
+                ))}
             </div>
             <ImageUpload id={params.restaurantId} item={false} restaurant={true} />
             <div className="text-lg">
