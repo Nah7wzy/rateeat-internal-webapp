6ff6318e6841e07ae6a0dd6dc18a0de93f764194
Abel Mesele <se.abel.mesele@gmail.com>
2024-01-24 19:31:43 +0300

    feat: Include status message

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -3,14 +3,45 @@ import { FormEvent, useState } from "react";
 
 export default function ImageUpload({ id, item, restaurant }: { id: string | string[] | undefined, item: boolean, restaurant: boolean }) {
     const [file, setFile] = useState<FileList>();
+    const [isDragging, setIsDragging] = useState(false);
+    const [uploadMessage, setUploadMessage] = useState("Upload Image/s");
 
     function handleChange(files: FileList | null) {
-        console.log(files);
         if (files) setFile(files);
+    }
+
+    function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(true);
+    }
+
+    function handleDragEnter(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(true);
+    }
+
+    function handleDragLeave(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(false);
+    }
+
+    function handleDrop(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(false);
+
+        const droppedFiles = e.dataTransfer.files;
+        if (droppedFiles.length > 0) {
+            setFile(droppedFiles);
+        }
     }
 
     function handleSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
+
         console.log(file);
         let formData = new FormData();
 
@@ -34,27 +65,36 @@ export default function ImageUpload({ id, item, restaurant }: { id: string | str
             body: formData,
         }).then((res) => {
             console.log(res);
+            if (res.status == 201) setUploadMessage("You did it! Upload Successful :)");
+            else setUploadMessage("Nah, try again. Upload Failed :(");
         }).catch((err) => {
             console.log(err);
         });
     }
+
     return (
         <>
-            <form onSubmit={handleSubmit} className="App">
-                <h2>Add Image:</h2>
-                <input type="file" onChange={(e) => handleChange(e.target.files)} multiple className="w-full h-24 bg-slate-200 rounded-lg"/>
+            <form
+                onSubmit={handleSubmit}
+                className={`${isDragging ? 'border-dashed border-2 border-[#4caf50]' : ''}`}
+                onDragOver={() => handleDragOver}
+                onDragEnter={() => handleDragEnter}
+                onDragLeave={() => handleDragLeave}
+                onDrop={() => handleDrop}
+            >
+                <h2>
+                    {uploadMessage}
+                </h2>
+                <input type="file" onChange={(e) => handleChange(e.target.files)} multiple className="w-full h-24 bg-slate-200 rounded-lg" />
 
                 <div className="flex gap-2">
-                    {
-                        file && Array.from(file).map((f, idx) => (
+                    {file &&
+                        Array.from(file).map((f, idx) => (
                             <img key={idx} src={file && URL.createObjectURL(f)} width={60} />
-                        ))
-                    }
+                        ))}
                 </div>
-                <button type="submit">
-                    SUBMIT
-                </button>
+                <button type="submit">SUBMIT</button>
             </form>
         </>
-    )
+    );
 }
