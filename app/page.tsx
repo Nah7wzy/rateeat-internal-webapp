8323fe9fb2c2811d0c51dfb07501ae6c30615527
Abel Mesele <se.abel.mesele@gmail.com>
2024-01-28 22:33:11 +0300

    fix: Add links to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      RateEat Internal Dashboard
+    <div className="bg-white flex flex-col items-center">
+      <div className="mb-">
+        RateEat Internal Dashboard
+      </div>
+      <div className="flex flex-col gap-4 items-center">
+        <Link href="/all_restaurants" className="hover:bg-blue-100 p-5 text-black"> All Restaurants</Link>
+        <Link href="/users" className="hover:bg-blue-100 p-5 text-black"> All Users</Link>
+      </div>
     </div>
   );
 }
